a7a7d2537e03a2aa378a755510ca04b45aaefd1e
Sam <sam@dev.com>
2024-05-24 13:19:36 +0200

    theme

--- content/keyboard-review.md
+++ content/keyboard-review.md
@@ -1,5 +1,8 @@
 title: My First Review
 Date: 2010-12-03 10:20
 Category: Review
+SubTitle: The medium is the message
+Cover: https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Hubble2005-01-barred-spiral-galaxy-NGC1300.jpg/1920px-Hubble2005-01-barred-spiral-galaxy-NGC1300.jpg
+Thumbnail: https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Hubble2005-01-barred-spiral-galaxy-NGC1300.jpg/800px-Hubble2005-01-barred-spiral-galaxy-NGC1300.jpg
 
 Following is a review of my favorite mechanical keyboard.
--- pelicanconf.py
+++ pelicanconf.py
@@ -8,6 +8,8 @@ TIMEZONE = 'Europe/Rome'
 
 DEFAULT_LANG = 'en'
 
+THEME = 'medius'
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = "feeds/all.atom.xml"
 CATEGORY_FEED_ATOM = None
@@ -31,6 +33,12 @@ SOCIAL = (
 
 DEFAULT_PAGINATION = False
 
+MEDIUS_CATEGORIES = {
+    'Review': {
+        'description': 'A galaxy is a gravitationally bound system of stars, stellar remnants, interstellar gas and dust, and dark matter.',
+        'logo': 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/GalacticRotation2.svg/250px-GalacticRotation2.svg.png',
+        'thumbnail': 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/NGC_3923_Elliptical_Shell_Galaxy.jpg/220px-NGC_3923_Elliptical_Shell_Galaxy.jpg'
+    }
+}
+
 # Uncomment following line if you want document-relative URLs when developing
-SITEURL = "https://samreghenzi.it/test-blog/"
-RELATIVE_URLS = False
